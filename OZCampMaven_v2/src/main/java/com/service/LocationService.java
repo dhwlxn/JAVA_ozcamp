0baa4e1637b350e3a623ae52769db8e6a5f8b2d9
joooohyung <comtergamee@gmail.com>
2023-05-02 11:04:21 +0900

    test

--- OZCampMaven_v2/src/main/java/com/service/LocationService.java
+++
@@ -1,9 +0,0 @@
-package com.service;
-
-import com.dto.LocationDTO;
-
-import java.util.List;
-
-public interface LocationService {
-    LocationDTO findByDistrict(LocationDTO dto);
-}
--- OZCampMaven_v2/src/main/java/menu/Board.java
+++
@@ -1,9 +0,0 @@
-package menu;
-
-public abstract class Board {
-
-	public abstract void insert(int CUID, int CID);
-	public abstract void delete(int CUID, int CID);
-	public abstract void findAll(int CUID);
-	
-}
